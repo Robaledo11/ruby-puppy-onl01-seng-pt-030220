ae3779093c6b17a15811be39cf00710b9f93e9b9
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-29 02:20:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,35 +1,31 @@
-# Add your code here
-
-class Dog 
-
- @@all = []
- 
- attr_accessor :name
- 
- def intitializes(name)
-   @name=name
-   @@all << self
+class Dog
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    self.save
   end
-
- def self.all 
-  @@all
-end 
-
-
-def self.print_all
-  @@all.each do |dog|
-    puts dog.name 
+  
+  def self.all
+    @@all
   end
-    
-    def save
-      @all << self
-      
-    def name
-      @name 
-    end
-    
+  
   def self.clear_all
-    @@all = []
-end    
+    @@all = [] 
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+end
+  
+  def save
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
 end
     
